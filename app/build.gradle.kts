81425462c6fc6974a967c7b27ddae4f1ae5c8b23
Gayathri Arumugam <gayathri.arumugam@sainsburys.co.uk>
2024-11-27 18:26:54 +0000

    Update applicationId

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.jokesapp"
+        applicationId = "com.sagayathri.jokesapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
